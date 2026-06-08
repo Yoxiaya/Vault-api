ed2fe7324359818a8ab04994ed336c216c1b8c85
Yoxia <171149510@qq.com>
2026-06-08 18:14:55 +0800

    style: 统一代码中的引号风格为单引号
    调整了src/index.ts文件内所有导入语句和方法调用的引号，将双引号替换为单引号，统一代码格式风格

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { vaultAccountsRoutes } from "./routes/vault-accounts.routes";
-import { profileRoutes } from "./routes/profile.routes";
-import { Bindings, Variables } from "./types/index";
-import { errorHandler } from "./middlewares";
-import { authRoutes } from "./routes/auth.routes";
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { vaultAccountsRoutes } from './routes/vault-accounts.routes';
+import { profileRoutes } from './routes/profile.routes';
+import { Bindings, Variables } from './types/index';
+import { errorHandler } from './middlewares';
+import { authRoutes } from './routes/auth.routes';
 
 const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
 
-app.use("*", logger());
-app.use("*", cors());
-app.use("*", errorHandler());
+app.use('*', cors());
+app.use('*', logger());
+app.use('*', errorHandler());
 
-app.route("/vault-accounts", vaultAccountsRoutes);
-app.route("/auth", authRoutes);
-app.route("/user", profileRoutes);
+app.route('/vault-accounts', vaultAccountsRoutes);
+app.route('/auth', authRoutes);
+app.route('/user', profileRoutes);
 
-app.get("/", (c) => c.json({ status: "ok", message: "Vault API is running" }));
+app.get('/', (c) => c.json({ status: 'ok', message: 'Vault API is running' }));
 
 export default app;
