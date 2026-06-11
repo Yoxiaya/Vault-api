324774f724a441767cb6d68711b291e4ef0f424b
Yoxia <171149510@qq.com>
2026-06-11 15:10:09 +0800

    chore: 新增本地开发端口8081到CORS允许源列表
    补充配置了本地开发使用的8081端口跨域访问权限，方便多前端开发环境调试

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const CORS_ORIGIN = ['https://vaultweb.yoxiaya.com', 'http://localhost:5173'];
+export const CORS_ORIGIN = ['https://vaultweb.yoxiaya.com', 'http://localhost:5173', 'http://localhost:8081'];
